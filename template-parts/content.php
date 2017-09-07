9d4a05102ab41739a93e606e8f4996d49505760d
Ken Zirkel <ken@zirkel.com>
2017-09-06 21:12:56 -0400

    archives update

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,41 +10,53 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-	    <?php humescores_the_category_list(); ?>
-	
-	
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	<div class="post__content">
+        <header class="entry-header">
+            <?php humescores_the_category_list(); ?>
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php humescores_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'humescores' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+            <?php
+            if ( is_single() ) :
+                the_title( '<h1 class="entry-title">', '</h1>' );
+            else :
+                the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            endif;
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'humescores' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+            if ( 'post' === get_post_type() ) : ?>
+            <div class="entry-meta">
+                <?php humescores_posted_on(); ?>
+            </div><!-- .entry-meta -->
+            <?php
+            endif; ?>
+        </header><!-- .entry-header -->
 
-	<footer class="entry-footer">
-		<?php humescores_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+        <div class="entry-content">
+            <?php
+                the_content( sprintf(
+                    /* translators: %s: Name of current post. */
+                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'humescores' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                ) );
+
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'humescores' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
+
+        <div class="continue-reading">
+           <?php $read_more_link = sprintf(
+                    /* translators: %s: Name of current post. */
+                    wp_kses( __( 'Continue reading %s <span class="meta-nav"></span>', 'humescores' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                );
+            ?>
+    
+            <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark"><?php echo $read_more_link; ?> <i class="fa fa-arrow-circle-o-right"></i></a>
+            
+            
+        </div>
+
+	</div> <!-- .post__content -->
 </article><!-- #post-## -->
